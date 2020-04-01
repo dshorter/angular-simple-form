fb002953f435df3a639d55ed087876c76cd7f45e
dshorter <codesurfer@gmail.com>
2020-04-01 17:19:49 -0400

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HelloComponent } from './hello.component';    
+import { SimpleFormComponent } from '../../simple-form/simple-form.component';    
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, SimpleFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
