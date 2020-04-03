1239f8641db73a78729d0015e736463cdd06a276
dshorter <codesurfer@gmail.com>
2020-04-03 13:31:32 -0400

    added validators folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {FormsModule} from '@angular/forms';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
-}) 
+})  
 export class AppComponent  {
   name = 'Angular';
 }
